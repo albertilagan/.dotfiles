dd60094c39920d383a206fdf7e7767784fc8a53c
Albert Ilagan <me@albertilagan.dev>
2024-08-26 17:03:34 +0800

    make it snappy

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -4,14 +4,9 @@ update() {
   source "$HOME/.config/sketchybar/colors.sh"
   
   sketchybar --set space.$AEROSPACE_FOCUSED_WORKSPACE  background.border_color=$ICON_COLOR background.color=$ICON_COLOR icon.color=$ICON_HIGHLIGHT_COLOR display=1
-  
-  if [ "$AEROSPACE_FOCUSED_WORKSPACE" == "$AEROSPACE_PREV_WORKSPACE" ]; then
-    return
-  fi
+  sketchybar --set space.$AEROSPACE_PREV_WORKSPACE  background.border_color=$BACKGROUND_SPACE background.color=$BACKGROUND_SPACE icon.color=$ICON_COLOR
 
-  if [ "$(aerospace list-windows --workspace $AEROSPACE_PREV_WORKSPACE)" != "" ]; then
-    sketchybar --set space.$AEROSPACE_PREV_WORKSPACE  background.border_color=$BACKGROUND_SPACE background.color=$BACKGROUND_SPACE icon.color=$ICON_COLOR
-  else
+  if [ "$(aerospace list-windows --workspace $AEROSPACE_PREV_WORKSPACE)" == "" ]; then
     sketchybar --set space.$AEROSPACE_PREV_WORKSPACE  background.border_color=$BACKGROUND_SPACE background.color=$BACKGROUND_SPACE icon.color=$ICON_COLOR display=0
   fi
 
