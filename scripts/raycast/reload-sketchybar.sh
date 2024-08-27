1282be3b12ac08d0959d1c7e6f8ede36db5746b9
Albert Ilagan <me@albertilagan.dev>
2024-08-27 15:54:28 +0800

    update aerospace config

---
+++ scripts/raycast/reload-sketchybar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Reload Sketchybar
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description Reload Sketchybar Config
+# @raycast.author albertilagan
+# @raycast.authorURL https://raycast.com/albertilagan
+
+sketchybar --reload
