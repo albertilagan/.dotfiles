28b7b400ba1e3a2f8329b39656fc227e541965e0
Albert Ilagan <me@albertilagan.dev>
2025-04-19 10:00:14 +0800

    chore: markdown

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -6,6 +6,27 @@ return {
     ---@type render.md.UserConfig
     opts = {
       render_modes = { 'n', 'c', 't' },
+      completions = { blink = { enabled = true } },
+      heading = {
+        width = 'block',
+        left_pad = 1,
+        right_pad = 1,
+      },
+      code = {
+        render_modes = true,
+        style = 'full',
+        language_pad = 2,
+        language_icon = false,
+        language_name = true,
+        disable_background = false,
+        width = 'block',
+        left_pad = 2,
+        right_pad = 2,
+        min_width = 120,
+        border = 'thin',
+        above = '▀',
+        below = '▀',
+      },
     },
   },
 }
