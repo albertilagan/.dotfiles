577babe9acfd19c4446ab491017ff37858d5cf09
Albert Ilagan <me@albertilagan.dev>
2025-06-12 22:57:17 +0800

    chore: revert to supermaven

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,32 +1,32 @@
--- return {
---   'supermaven-inc/supermaven-nvim',
---   config = function()
---     require('supermaven-nvim').setup {}
---   end,
--- }
 return {
-  {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter',
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = '<Tab>',
-          next = '<M-]>',
-          prev = '<M-[>',
-          dismiss = '<C-]>',
-        },
-      },
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-      },
-      filetypes = {
-        ['*'] = true,
-      },
-    },
-  },
+  'supermaven-inc/supermaven-nvim',
+  config = function()
+    require('supermaven-nvim').setup {}
+  end,
 }
+-- return {
+--   {
+--     'zbirenbaum/copilot.lua',
+--     cmd = 'Copilot',
+--     event = 'InsertEnter',
+--     opts = {
+--       suggestion = {
+--         enabled = true,
+--         auto_trigger = true,
+--         keymap = {
+--           accept = '<Tab>',
+--           next = '<M-]>',
+--           prev = '<M-[>',
+--           dismiss = '<C-]>',
+--         },
+--       },
+--       panel = {
+--         enabled = true,
+--         auto_refresh = true,
+--       },
+--       filetypes = {
+--         ['*'] = true,
+--       },
+--     },
+--   },
+-- }
