037dfb44716635b00c6a419dd3343e16f25204b6
Albert Ilagan <me@albertilagan.dev>
2025-06-20 16:42:45 +0800

    fix: optimize fish conf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,11 +9,14 @@ zoxide init fish | source
 
 # fish_add_path ~/.nix-profile/bin
 fish_add_path ~/.config/bin
-fish_add_path /usr/local/go/bin
+fish_add_path ~/.lmstudio/bin
+fish_add_path ~/.local/bin
 fish_add_path ~/.locize-cli/bin
+fish_add_path ~/.opencode/bin
+fish_add_path ~/miniforge3/bin
+fish_add_path /usr/local/go/bin
 fish_add_path /opt/homebrew/opt/fzf/bin
 fish_add_path /Applications/Ghostty.app/Contents/MacOS
-fish_add_path ~/miniforge3/bin
 
 set -U fish_greeting # disable fish greeting
 set -U fish_key_bindings fish_vi_key_bindings # fish_default_key_bindings
@@ -22,39 +25,8 @@ set -x VISUAL nvim
 set -x EDITOR nvim
 set -x CONFIG_DIR "$HOME/.config/lazygit"
 
-source "$GHOSTTY_RESOURCES_DIR"/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish
 source ~/.config/fish/keys.fish
 source ~/.config/fish/fzf.fish
 source ~/.config/fish/git.fish
 source ~/.config/fish/misc.fish
 source ~/.config/fish/kubernetes.fish
-test -f ~/.config/fish/kubernetes.fish && source \
-     (cat ~/.config/fish/kubernetes.fish | sed -r 's/(kubectl.*) --watch/watch \1/g' | psub)
-# source ~/.config/fish/carapace.fish
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-# Created by `pipx` on 2025-04-24 16:18:34
-set PATH $PATH /Users/albert/.local/bin
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/albert/miniforge3/bin/conda
-    eval /Users/albert/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/Users/albert/miniforge3/etc/fish/conf.d/conda.fish"
-        . "/Users/albert/miniforge3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/Users/albert/miniforge3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/albert/.lmstudio/bin
-
-# opencode
-fish_add_path /Users/albert/.opencode/bin
