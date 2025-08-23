7e08d1818cbee2d2c61a402dc32b0f239bf42ad0
Albert Ilagan <me@albertilagan.dev>
2025-08-24 07:31:53 +0800

    nvim: rest json formatting

--- nvim/.config/nvim/lua/plugins/rest.lua
+++ nvim/.config/nvim/lua/plugins/rest.lua
@@ -7,4 +7,19 @@ return {
       table.insert(opts.ensure_installed, 'http')
     end,
   },
+  config = function()
+    require('rest-nvim').setup {
+      env = {
+        pattern = '%.env$',
+      },
+      ui = {
+        keybinds = {
+          prev = 'P',
+          next = 'N',
+        },
+      },
+    }
+    vim.keymap.set('n', '<leader>rr', '<cmd>Rest run<CR>', { desc = 'Run rest command' })
+    vim.keymap.set('n', '<leader>rl', '<cmd>Rest run last<CR>', { desc = 'Run last rest command' })
+  end,
 }
